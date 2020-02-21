9d46a74f816ec207e7c4c3781b3893d3603d7369
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-02-21 14:02:46 -0500

    have a plan, not able to implement correctly

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -32,6 +32,13 @@ class Stack():
             return None
     def size(self):
         return len(self.stack)
+    
+# def get_neighbors(exit):
+#     """
+#     Get all neighbors (edges) of a vertex.
+#     """
+#     p = {}
+#     return p['exit']
 
 # Load world
 world = World()
@@ -55,28 +62,79 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
 # -------------------------------------
 
+# player is the Node 
+# rooms are the Edges
+# (0 - 500)
 
 traversal_path = []
-queue = []
+visited = []
 
-def bfs(visited, graph, node):
-    visited.append(node)
-    queue.append(node)
+def explore_all(starting_point):
+    s = Stack()
+    for path in traversal_path:
+        player.travel('n')
+        
 
-    while queue:
-        s = queue.pop(0)
-        print(s, end = ' ')
+# use DST to pick a unvisted direction from players room
+# for path in traval_path
+    # pick unvisted direction (n/s/e/w)
+    # player.travel into room 
+    # add it to visted
+    # if unvisted direction is none
+        # player.travel to nearest unvisted room
 
-        for neighbor in graph[s]:
-            if neighbor not in visited:
-                visited.append(neighbor)
-                queue.append(neighbor)
+# use BFS to search for the shortest unvisted room
+# searching for an exit with a `'?'` as the value
+#  if exit is in visted
+    # add exit to the queue  
 
-bfs(traversal_path, queue, '0')
+# convert list of rooms to a list of n/s/e/w
+# add to traversal_list
+# if all paths explored, done
 
 
+# -------------------------------------
+
+# traversal_path = []
+
+
+
+
+# class Graph:
+#     def __int__(self):
+#         self.exit = {}
+        
+#     def get_neighbors(self, exits):
+#         return self.exit(exits)
+    
+#     def dft(self, starting_dir):
+#         s = Stack()
+#         # pick an unexplored direction from the player's current room
+#         s.push(starting_dir)
+#         visited = set()
+#         while s.size() > 0:
+#             v = s.pop()
+#             if v not in visited:
+#                 print(v)
+#                 visited.add(v)
+#                 for neighbor in self.get_neighbors(v):
+#                     s.push(neighbor)
+
+# travel and log that direction
+# traversal_path.append(paths[0])
+# # path = traversal_path[paths[0]]
+# # print(path)
+# # then loop
+# for path in traversal_path:
+#     # When you reach a dead-end
+#     if world.rooms == '?':
+#         # walk back to the nearest room that does contain an unexplored path
+#         traversal_path.pop()
+
+        
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
